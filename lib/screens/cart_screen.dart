580a3ef0a279cb9efe9ad7403a39778e64143309
Sayantan <sayantan.dasgupta21@gmail.com>
2021-10-30 12:06:43 +0530

    Added Expandable Behaviour to Orders

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -35,7 +35,7 @@ class CartScreen extends StatelessWidget {
                   Spacer(),
                   Chip(
                     label: Text(
-                      "\$ ${cart.totalAmount}",
+                      "\$ ${cart.totalAmount.toStringAsFixed(2)}",
                       style: TextStyle(
                         color:
                             Theme.of(context).primaryTextTheme.headline6!.color,
--- lib/widgets/order_card.dart
+++ lib/widgets/order_card.dart
@@ -1,11 +1,20 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:shopify_app/providers/orders.dart';
 
-class OrderCard extends StatelessWidget {
+class OrderCard extends StatefulWidget {
   OrderCard({required this.order});
 
   final OrderItem order;
+
+  @override
+  State<OrderCard> createState() => _OrderCardState();
+}
+
+class _OrderCardState extends State<OrderCard> {
+  bool _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -15,16 +24,50 @@ class OrderCard extends StatelessWidget {
         children: <Widget>[
           ListTile(
             title: Text(
-              '\$ ${order.total}',
+              '\$ ${widget.order.total}',
             ),
             subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime),
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
             ),
             trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
             ),
           ),
+          if (_expanded)
+            Container(
+              height: min(widget.order.cartItems.length * 20.0 + 50, 180),
+              padding: EdgeInsets.all(15.0),
+              child: ListView(
+                children: widget.order.cartItems
+                    .map(
+                      (e) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "${e.title}",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          Text(
+                            "${e.quantity}x \$${e.price}",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
         ],
       ),
     );
